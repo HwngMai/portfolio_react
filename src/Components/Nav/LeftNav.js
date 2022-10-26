cd197071d18fc52c834e816502e5dca1824e0d33
Hung Mai <hwngmvi@gmail.com>
2022-10-26 16:29:27 +0700

    commit

--- src/Components/Nav/LeftNav.js
+++ src/Components/Nav/LeftNav.js
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 
 export default function LeftNav() {
   return (
-    <div className='leftNav flex z-10 m-auto justify-start items-center fixed mt-20 sm:-left-36 md:-left-32 gap-5 left'>
+    <div className='leftNav flex z-10 m-auto justify-start items-center fixed mt-10 sm:-left-36 md:-left-32 gap-5 left'>
       {" "}
       <Link to='feature' offset={-10} smooth={true}>
         <span className='text-base tracking-wider hover:text-red-500 text-gray-500 transition duration-300'>
--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -15,15 +15,17 @@ export default function Scroll() {
       <Link to='carousel' smooth={true}>
         <div className='scrollNav z-30 w-28 h-28 container rotate-90 fixed bottom-10 md:right-0 sm:-right-10 flex justify-end items-center gap-4'>
           <div className='scrollLineBack w-2 bg-white'></div>
-          <p className='text-white'>Up!</p>
+          <div className='carousel__up border hover:border-red-500 transition rounded-full hover:text-red-500 w-12 h-12 flex justify-center items-center'>
+            <p className='text-white text-center'>Up!</p>
+          </div>
         </div>
       </Link>
     );
   } else {
     return (
       <div className='scrollNav z-30 w-28 h-28 container rotate-90 fixed bottom-10 md:right-0 sm:-right-10 flex justify-between items-center gap-1'>
-        <div className='scrollLine w-2 bg-gray-500'></div>
-        <p className='text-gray-500'>Scroll</p>
+        <div className='scrollLine w-2 bg-gray-600'></div>
+        <p className='text-gray-600'>Scroll</p>
       </div>
     );
   }
