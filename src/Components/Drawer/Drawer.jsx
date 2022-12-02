3051b27e21f7c67ba1d8e57820525da7880e4230
Hung Mai <hwngmvi@gmail.com>
2022-12-02 15:09:19 +0700

    commit

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Drawer() {
-  return <> Drawer </>;
+  return (
+    <>
+      <p className='text-white'> this is drawer </p>{" "}
+      <p className='text-white'>
+        {" "}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt fugit
+        nam dolorem voluptates provident optio doloribus ipsa quaerat nihil, rem
+        odio atque sit velit voluptatem excepturi illum neque inventore natus
+        sint tempora? Beatae accusamus esse harum nesciunt totam delectus,
+        similique fuga quidem ab sequi voluptatum est recusandae! Ipsa rerum
+        ipsam consequatur temporibus! Numquam, vel itaque adipisci in pariatur
+        esse accusantium a corrupti, voluptas et debitis molestias dolore atque
+        exercitationem sapiente nesciunt perferendis quam reiciendis ducimus
+        omnis voluptate quo ab accusamus beatae. Tempore assumenda tempora
+        voluptates blanditiis dolor reprehenderit neque commodi numquam autem,
+        error corrupti iusto itaque aliquid non adipisci aliquam.{" "}
+      </p>{" "}
+    </>
+  );
 }
 
 export default Drawer;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
     <div
       className={`${
         header ? "justify-between" : "justify-end"
-      } text-white z-40 w-full h-10 sm:px-10 md:px-20 header mx-auto fixed mt-10 top-0 flex  items-center`}>
+      } header text-white z-40 w-full h-10 sm:px-10 md:px-20  mx-auto fixed mt-10 top-0 flex  items-center`}>
       <div className={`${header ? "" : "hidden "}  header__logo`}>
         <p
           className={`${
@@ -56,16 +56,10 @@ const Header = () => {
         <div
           className={`header__nav__button ${
             trigger ? "bubble-full" : "bubble-full-re"
-          } absolute right-0 cursor-pointer h-10 w-10 flex items-center z-40 justify-center rounded-full bg-red-500`}>
+          } absolute right-0 overflow-hidden cursor-pointer h-10 w-10 flex items-center z-40 justify-center rounded-full bg-red-500`}>
           <button className='z-40' onClick={handleOpenDrawer}>
             <BiAlignRight size={25} />
           </button>
-          <div
-            className={`h-screen w-screen ${
-              openDrawer ? "" : "hidden"
-            }  -top-10 -right-16 bg-red-500 z-40 absolute`}>
-            <Drawer />
-          </div>
         </div>
       </div>
     </div>
